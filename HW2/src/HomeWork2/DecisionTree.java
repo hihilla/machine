b07f0febe3de16e6c7f3304b7657cfa9894e1a21
Hilla Barkal <hilla.guz@gmail.com>
2017-05-05 23:32:13 +0300

    making methods private

--- HW2/src/HomeWork2/DecisionTree.java
+++ HW2/src/HomeWork2/DecisionTree.java
@@ -58,7 +58,7 @@ public class DecisionTree implements Classifier {
      * queue algorithm.
      * @param instances
      */
-    public void buildTree(Instances instances) {
+    private void buildTree(Instances instances) {
     	
     }
     
@@ -70,7 +70,7 @@ public class DecisionTree implements Classifier {
      * @param instance
      * @return Average error 
      */
-    public double calcAvgError(Instance instance){
+    private double calcAvgError(Instance instance){
     	return 0;
     }
     
@@ -80,7 +80,7 @@ public class DecisionTree implements Classifier {
      * @param instances
      * @return The information gain 
      */
-    public double calcInfoGain(Instances instances){
+    private double calcInfoGain(Instances instances){
     	return 0;
     }
     /**
@@ -91,7 +91,7 @@ public class DecisionTree implements Classifier {
      * @param instances set of instances (in certain node, probably)
      * @return probabilities of positive class (1) for each possible attribute
      */
-    public double[] calcPositiveProbabilities(Instances instances){
+    private double[] calcPositiveProbabilities(Instances instances){
     	int numOfAttributes = instances.numAttributes();
     	int numOfInstances = instances.numInstances();
     	int classIndex = instances.classIndex();
@@ -125,7 +125,7 @@ public class DecisionTree implements Classifier {
      * @param probabilities - A set of probabilities
      * @return The entropy 
      */
-    public double calcEntropy(double[] probabilities){
+    private double calcEntropy(double[] probabilities){
     	int numOfInstances = probabilities.length;
     	double entropy;
     	double probOfCurrEvent;
@@ -156,7 +156,7 @@ public class DecisionTree implements Classifier {
      * @param attributeIndex
      * @return The chi square score
      */
-    public double calcChiSquare(Instances instances, int attributeIndex){
+    private double calcChiSquare(Instances instances, int attributeIndex){
     	// xj is the attribute at index j (attributeIndex)
     	int numValues = instances.attribute(attributeIndex).numValues();
     	int numInstances = instances.numInstances();
@@ -220,11 +220,26 @@ public class DecisionTree implements Classifier {
      * to prune a branch of the tree or not. We compare resulted Chi square
      * with number from chi squared chart in the row for 8 degrees of freedom 
      * (which is the number of attributes in the cancer data minus 1) and the 
-     * column for 0.95 confidence level
+     * column for 0.95 confidence level.
      */
     private void chiSquarePrunning(){
     	double CHI_SQUARE_LIMIT = 15.51;
-    	
+    	//PAY ATTENTION – where you need to perform this test, what you should do if the result is to prune.
+
+    }
+    
+    /**
+     * Prunning the tree by checking if removing a rule improve the result.
+     * After complete building the tree you will go over all the rules and 
+     * check if removing a rule will improve the error on the validation set.
+     * Pick the best rule to remove according to the error on the validation 
+     * set and remove it from the rule set. Stop removing rules when there is 
+     * no improvement.
+     * @param validationSet
+     */
+    private void rulePrunning(Instances validationSet){
+//    	PAY ATTENTION – for how you loops over the rule, how you remove rules during this loop, how you decide to stop.
+
     }
     
     @Override
